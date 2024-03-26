20af0fae025d021d7f4d278bbf53b4d2fbea366b
ziliwalid <walidzili19@gmail.com>
2024-03-26 15:48:15 +0000

    WIP / Adding Home page+Layout+animating letters

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 //import logo from './logo.svg';
 import './App.scss';
 import Layout from './components/Layout';
+import Home from './components/Home'
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     /*Defining routes of the app*/
-    <Routes>
-      <Route path="/" element={<Layout />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,19 +1,25 @@
+/*this is where the layout is set up
+the layout : <body></body>[...]
+just for the aesthetic
+*/
 import { Outlet } from 'react-router-dom';
-import Sidebar from './Sidebar';
+import Sidebar from './../Sidebar';
 import './index.scss';
-const Layout = () =>{
-    return <div className='App'>
+const Layout = () => {
+    return (
+      <div className="App">
         <Sidebar />
-            <div className='page'>
-                <span className='tags top-tags'>&lt;body&gt;</span>
-                <Outlet/>
-                <span className='tags bottom-tags'>
-                    &lt;/body&gt;
-                </span>
-                <br/>
-                <span className='bottom-tag-html'>&lt;/html&gt;</span>
-            </div>
+        <div className="page">
+          <span className="tags top-tags">&lt;body&gt;</span>
+          <Outlet />
+          <span className="tags bottom-tags">
+            &lt;/body&gt;
+            <br />
+            <span className="bottom-tag-html">&lt;/html&gt;</span>
+          </span>
         </div>
-}
+      </div>
+    )
 
+    }
 export default Layout;
