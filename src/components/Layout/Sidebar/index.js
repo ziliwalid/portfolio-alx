f927005e91dd92cc18d3db3f126e122d0639830e
ziliwalid <walidzili19@gmail.com>
2024-03-26 10:45:22 +0000

    WIP / initiating creation of nav bar and other components

---
+++ src/components/Layout/Sidebar/index.js
@@ -0,0 +1,17 @@
+import './index.scss';
+
+const Layout = () => {
+    return (
+        <div className='nav-bar'>
+            {/* Logo */}
+            <div className="logo">
+                <span className="logo-line">W</span>
+                <br />
+                <span className="logo-line">Walid</span>
+            </div>
+            {/* Nav content */}
+        </div>
+    );
+}
+
+export default Layout;
---
+++ src/components/Layout/index.js
@@ -0,0 +1,7 @@
+import Sidebar from './Sidebar';
+import './index.scss';
+const Layout = () =>{
+    return <><Sidebar /></>
+}
+
+export default Layout;
