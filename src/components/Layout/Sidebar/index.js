206fad635c33405487c7f958fe67bd1eb7d9c740
ziliwalid <walidzili19@gmail.com>
2024-03-26 12:09:44 +0000

    WIP / navbar in dev

--- src/components/Layout/Sidebar/index.js
+++ src/components/Layout/Sidebar/index.js
@@ -1,4 +1,8 @@
+import { NavLink } from 'react-router-dom';
 import './index.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Layout = () => {
     return (
@@ -10,6 +14,29 @@ const Layout = () => {
                 <span className="logo-line">Walid</span>
             </div>
             {/* Nav content */}
+            <nav>
+                <NavLink exact="true" activeclassname="active" to="/">
+                    <FontAwesomeIcon icon={faHome} color='white'/>
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                    <FontAwesomeIcon icon={faUser} color='white'/>
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                    <FontAwesomeIcon icon={faEnvelope} color='white'/>
+                </NavLink>
+            </nav>
+            <ul>
+                <li>
+                    <a target='_blanl' rel='noreferrer' href='https://www.linkedin.com/in/walid-zili-b042921b2/'>
+                        <FontAwesomeIcon icon={faLinkedin} color='white'/>
+                    </a>
+                </li>
+                <li>
+                    <a target='_blanl' rel='noreferrer' href='https://github.com/ziliwalid'>
+                        <FontAwesomeIcon icon={faGithub} color='white'/>
+                    </a>
+                </li>
+            </ul>
         </div>
     );
 }
